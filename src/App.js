16ad459840676aacddc16002e17d6b68bb6d73bf
petersenea <Petersenea12@gmail.com>
2020-04-08 16:18:13 -0500

    slice 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
-import {Button, Container, Title} from 'rbx';
+import { Button, Container, Title } from 'rbx';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -29,17 +29,17 @@ const schedule = {
 
 };
 
-const Banner = ({title}) => (
-  <Title>{title}</Title>
+const Banner = ({ title }) => (
+  <Title>{title || '[loading...]'}</Title>
 )
 
-const CourseList = ({courses}) => (
+const CourseList = ({ courses }) => (
   <Button.Group>
-    {courses.map(course => <Course key={course.id} course = {course}/>)}
+    {courses.map(course => <Course key={course.id} course={course} />)}
   </Button.Group>
 )
 
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring' };
 
 const getCourseTerm = course => (
   terms[course.id.charAt(0)]
@@ -48,18 +48,35 @@ const getCourseTerm = course => (
 const getCourseNumber = course => (
   course.id.slice(1, 4)
 )
-  
+
 const Course = ({ course }) => (
   <Button>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+    {getCourseTerm(course)} CS { getCourseNumber(course)}: { course.title}
   </Button>
 );
 
-const App = () => (
-  <Container>
-    <Banner title={schedule.title} />
-    <CourseList courses={schedule.courses} />
-  </Container>
-);
+
+
+const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+  
+  return (
+    <Container>
+      <Banner title={schedule.title} />
+      <CourseList courses={schedule.courses} />
+    </Container>
+  );
+};
 
 export default App;
